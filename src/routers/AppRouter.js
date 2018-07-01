d9171dca9b412aa875b6555853665e62fa5ae18d
Jonathan Berthier <andomorphia@gmail.com>
2018-07-02 01:06:47 +0200

    Fixed page scroll and component animations on history changes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,29 +7,32 @@ import Header from '../components/Header';
 import RecipesList from '../containers/RecipesList';
 import Recipe from '../containers/Recipe';
 import NotFoundPage from '../components/NotFoundPage';
+import ScrollToTop from '../components/ScrollToTop';
 
 const AppRouter = () => (
   <Router>
-    <Route render={({ location }) => (
-      <Fragment>
-        <Header />
-        <div className="container">
-          <TransitionGroup>
-            <CSSTransition
-              key={location.key}
-              timeout={250}
-              classNames="fade"
-            >
-              <Switch location={location}>
-                <Route exact path="/" component={RecipesList} />
-                <Route path="/recipe/:slug" component={Recipe} />
-                <Route component={NotFoundPage} />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        </div>
-      </Fragment>
-    )} />
+    <ScrollToTop>
+      <Route render={({ location }) => (
+        <Fragment>
+          <Header />
+          <div className="container">
+            <TransitionGroup>
+              <CSSTransition
+                key={location.key}
+                timeout={250}
+                classNames="fade"
+              >
+                <Switch location={location}>
+                  <Route exact path="/" component={RecipesList} />
+                  <Route path="/recipe/:slug" component={Recipe} />
+                  <Route component={NotFoundPage} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          </div>
+        </Fragment>
+      )} />
+    </ScrollToTop>
   </Router>
 );
 

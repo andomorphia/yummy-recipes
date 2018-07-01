5b5aa74319228355f2991f155df6cc848d032c63
Jonathan Berthier <andomorphia@gmail.com>
2018-07-01 19:39:02 +0200

    Playing around with react-transition-group

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 /*eslint-disable */
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import Header from '../components/Header';
 import RecipesList from '../containers/RecipesList';
@@ -9,16 +10,26 @@ import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
   <Router>
-    <Fragment>
-      <Header />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={RecipesList} />
-          <Route path="/recipe/:slug" component={Recipe} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
-    </Fragment>
+    <Route render={({ location }) => (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <TransitionGroup>
+            <CSSTransition
+              key={location.key}
+              timeout={250}
+              classNames="fade"
+            >
+              <Switch location={location}>
+                <Route exact path="/" component={RecipesList} />
+                <Route path="/recipe/:slug" component={Recipe} />
+                <Route component={NotFoundPage} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
+      </Fragment>
+    )} />
   </Router>
 );
 
